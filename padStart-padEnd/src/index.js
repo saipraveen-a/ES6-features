7e7629abc40ab18999c5e3154126bab048c38696
Sai Praveen Anumukonda <saipraveen-a@github.com>
2020-09-19 17:11:49 +0530

    Add another example

--- padStart-padEnd/src/index.js
+++ padStart-padEnd/src/index.js
@@ -4,3 +4,8 @@ let example = 'Sai';
 
 console.log(example.padStart(10, 'a'));
 console.log(example.padEnd(10, 'a'));
+
+let example1 = 'YouTube.com/CodingTutorials360';
+
+console.log(example1.padStart(100).length);
+console.log(example1.padEnd(1));
