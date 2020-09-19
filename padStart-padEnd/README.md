3eb950a4c0269f800613754042be050e0b16f52c
Sai Praveen Anumukonda <saipraveen-a@github.com>
2020-09-19 17:10:34 +0530

    padStart and padEnd

---
+++ padStart-padEnd/README.md
@@ -0,0 +1,5 @@
+# padStart and padEnd is ES6
+
+The code sample demonstrates 
+	
+	1.  Usage of padStart and padEnd in ES6. 
---
+++ padStart-padEnd/src/index.js
@@ -0,0 +1,6 @@
+let example = 'Sai';
+// let example = 'Sai Praveen';
+
+
+console.log(example.padStart(10, 'a'));
+console.log(example.padEnd(10, 'a'));
