b8c515f858633e7215dba951a9ddccde05cee449
Sai Praveen Anumukonda <saipraveen-a@github.com>
2020-09-21 11:06:37 +0530

    Classes - Another example

--- classes/src/index.js
+++ classes/src/index.js
@@ -17,3 +17,35 @@ console.log(cat.metaData)
 let newCat = new Cat('Cat', 4);
 newCat.makeNoise();
 console.log(newCat.metaData);
+
+
+class Player {
+    constructor(name, country) {
+        this.name = name;
+        this.country = country;
+    }
+    
+    introduce() {
+        console.log(`${this.name} was born in ${this.country}`);
+    }
+}
+
+const messi = new Player("Messi", "Argentina");
+
+messi.introduce();
+
+class TennisPlayer extends Player {
+    constructor(name, country, age) {
+        super(name, country);
+        this.age = age;
+    }
+    
+    playTennis() {
+        console.log(`${this.name} is ${this.age} years old and knows how to play Tennis`);
+    }
+}
+
+const roger = new TennisPlayer( "Roger Federer", "Switzerland", 38);
+
+roger.introduce(); // able to do this as the introduce method is available in the prototype(Player) of the object
+roger.playTennis();
